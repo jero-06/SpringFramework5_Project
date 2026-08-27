15334b6f0bca7557ba1eef842a32f9559ef002d2
jero-06 <jeeyoung345@gmail.com>
2026-08-27 16:37:26 +0900

    HelloBeanSpringTest add

---
+++ src/test/java/myspring/di/xml/HelloBeanSpringTest.java
@@ -0,0 +1,20 @@
+package myspring.di.xml;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:hello-bean.xml")
+public class HelloBeanSpringTest {
+	@Autowired
+	Hello hello;
+	
+	@Test
+	void helloBeanCons() {
+		
+	}
+
+}
