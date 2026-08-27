d587dd4b88ac935636615d42018d1b4aa57d87cb
jero-06 <jeeyoung345@gmail.com>
2026-08-27 14:39:19 +0900

    HelloBeanJunitTest add

--- src/test/java/myspring/di/xml/HelloBeanJunitTest.java
+++ src/test/java/myspring/di/xml/HelloBeanJunitTest.java
@@ -1,12 +1,15 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloBeanJunitTest {
 	
 	@Test
 	void helloBeanSetter() {
-		
+		//1. Spring Container 객체생성
+		ApplicationContext context = new GenericXmlApplicationContext("");
 	}
 
 }
