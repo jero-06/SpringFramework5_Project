b098c0bc479fc15e69242d071b6e4dcf31cb06aa
jero-06 <jeeyoung345@gmail.com>
2026-08-28 14:18:18 +0900

    HelloConfig add

--- src/main/java/myspring/di/xml/config/HelloConfig.java
+++ src/main/java/myspring/di/xml/config/HelloConfig.java
@@ -1,16 +1,22 @@
 package myspring.di.xml.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
 
 import myspring.di.xml.ConsolePrinter;
+import myspring.di.xml.Hello;
 import myspring.di.xml.Printer;
 import myspring.di.xml.StringPrinter;
 
 @Configuration
 @PropertySource(value="classpath:values.properties")
 public class HelloConfig {
+	@Autowired
+	Environment env;
 	
 	@Bean
 	public Printer stringPrinter() {
@@ -22,5 +28,24 @@ public class HelloConfig {
 	public Printer consoPrinter() {
 		return new ConsolePrinter();
 	}
+	
+	/*
+	 * <bean id="hello" class="myspring.di.xml.Hello" scope="singleton">
+		<!-- setName() 메서드 호출 -->
+		<property name="name" value="자바컨피그" />
+		<!-- setPrinter(new StringPrinter()) 메서드 호출 -->
+		<property name="printer" ref="strPrinter" />
+	</bean>
+	 */
+	@Bean @Scope(value = "singeton")
+	public Hello hello() {
+		Hello hello = new Hello();
+		// 자바컨피그
+		String propName = env.getProperty("myName3")
+		hello.setName(propName);
+		hello.setPrinter(stringPrinter());
+		return hello;
+	}
+	
 
 }
