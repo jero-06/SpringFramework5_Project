9011db08aaf2c750604202a0aca91f090a0d7b23
jero-06 <jeeyoung345@gmail.com>
2026-08-28 10:17:58 +0900

    HelloBeanCons add

---
+++ src/test/java/myspring/di/annot/AnnotatedHelloBeanTest.java
@@ -0,0 +1,35 @@
+package myspring.di.annot;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import myspring.di.annot.xml.HelloBean;
+import myspring.di.annot.xml.PrinterBean;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:hello-annot.xml")
+
+public class AnnotatedHelloBeanTest {
+	@Autowired
+	HelloBean hello;
+	
+	@Resource(name = "stringPrinter")
+	PrinterBean printer;
+	
+	@Test
+	void helloBeanAnnot() {
+		assertEquals("Hello 어노테이션", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 어노테이션", printer.toString());
+		
+		
+	}
+
+}
